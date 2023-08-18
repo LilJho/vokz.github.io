f3d7be83d6a2cdb4ee0be2b16b51d2f0377188d9
Ivan Hamid <fieryandfiend@gmail.com>
2023-08-18 21:30:26 +0800

    fix: logout bug

--- gaia/src/app/(routes)/layout.tsx
+++ gaia/src/app/(routes)/layout.tsx
@@ -1,16 +1,7 @@
-import NavLink from '@/components/ui/NavLink'
-import { routesConfig, Icons } from '@/config/routes'
 import React from 'react'
-import Image from 'next/image'
-import Logo from "@public/logo/logo.svg"
-import { RiArrowLeftSLine } from 'react-icons/ri'
-import { BiBell, BiSearch } from 'react-icons/bi'
-import { Button } from '@/components/ui/button'
 import { redirect } from 'next/navigation'
 import { createServerSupabaseClient } from "@/services/supabaseServer"
-import UserProfile from '@/components/layout/UserProfile'
 import Navigation from '@/components/layout/Navigation'
-import Header from '@/components/layout/Header'
 import MainContent from '@/components/layout/MainContent'
 
 interface ILayoutProps {
@@ -21,7 +12,7 @@ const MainPageLayout = async ({ children }: ILayoutProps) => {
     const supabase = createServerSupabaseClient()
 
     const { data: { session } } = await supabase.auth.getSession()
-
+    console.log({ session })
     if (!session) {
         redirect("/auth/sign-in")
     }
--- gaia/src/components/views/auth/Logout.tsx
+++ gaia/src/components/views/auth/Logout.tsx
@@ -5,14 +5,17 @@ import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
 import { useRouter } from 'next/navigation'
 import React from 'react'
 
+const supabase = createClientComponentClient();
+
 const Logout = ({ children = "Logout" }: { children: React.ReactNode }) => {
     const signOut = useUserStore((state) => state.signOut)
 
     const { push, refresh } = useRouter()
     const handleSignOut = async () => {
+        push("/auth/sign-in")
+        await supabase.auth.signOut()
         signOut()
         refresh()
-        push("/auth/sign-in")
     }
 
     return (
