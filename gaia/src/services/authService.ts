2680bf338ba81551068d70a5997df2a6d8369ccb
Ivan Hamid <fieryandfiend@gmail.com>
2023-08-19 04:41:42 +0800

    Update authService.ts

--- gaia/src/services/authService.ts
+++ gaia/src/services/authService.ts
@@ -1,5 +1,4 @@
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
-import { UserAccountsService } from "./databaseServices";
 import { IUserSessionData } from "@/lib/types";
 
 interface AuthValues {
@@ -14,7 +13,6 @@ const authService = async ({
   password,
 }: AuthValues): Promise<AuthServiceResponse> => {
   const supabase = createClientComponentClient();
-
   const { data: authData, error } = await supabase.auth.signInWithPassword({
     email,
     password,
@@ -25,7 +23,12 @@ const authService = async ({
   }
 
   const userData = authData.user;
-  const data: any = await UserAccountsService.getOne("uuid", userData.id);
+
+  let { data }: any = await supabase
+    .from("user_accounts")
+    .select("*")
+    .eq("uuid", userData.id);
+
   const user = {
     email: userData.email || "",
     first_name: data[0].first_name || "",
