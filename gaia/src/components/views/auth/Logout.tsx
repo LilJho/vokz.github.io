43b5c117d2ef84344107c248479c16c05ed1cc4d
Ivan Hamid <fieryandfiend@gmail.com>
2023-08-19 23:42:41 +0800

    Update Logout.tsx

--- gaia/src/components/views/auth/Logout.tsx
+++ gaia/src/components/views/auth/Logout.tsx
@@ -1,4 +1,5 @@
 "use client"
+import userStore from '@/lib/store/userStore';
 import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
 import { useRouter } from 'next/navigation'
 import React from 'react'
@@ -7,8 +8,10 @@ const supabase = createClientComponentClient();
 
 const Logout = ({ children = "Logout" }: { children: React.ReactNode }) => {
     const { push, refresh } = useRouter()
+
     const handleSignOut = async () => {
         await supabase.auth.signOut()
+        userStore.setState({ user: null })
         refresh()
         push("/auth/sign-in")
     }
